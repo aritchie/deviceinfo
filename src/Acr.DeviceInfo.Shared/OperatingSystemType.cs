415db8db057c27e5cb5bf93a2ba6e8393f4e1e3d
Allan Ritchie <allan.ritchie@gmail.com>
2015-04-21 10:59:08 -0400

    Bump version and missed file

---
+++ src/Acr.DeviceInfo.Shared/OperatingSystemType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Acr.DeviceInfo {
+
+    public enum OperatingSystemType {
+        Unknown = 0,
+        iOS = 1,
+        Android = 2,
+        WindowsPhone = 3,
+        Windows = 4
+    }
+}
