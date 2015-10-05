8a37a923ac6969635f359fb5363d98d23f1eeb63
Allan Ritchie <allan.ritchie@gmail.com>
2015-10-05 08:42:22 -0400

    Battery info for UWP

--- src/Acr.DeviceInfo.Uwp/BatteryImpl.cs
+++ src/Acr.DeviceInfo.Uwp/BatteryImpl.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Windows.Devices.Power;
+using Windows.System.Power;
 
 
 namespace Acr.DeviceInfo {
@@ -6,6 +8,17 @@ namespace Acr.DeviceInfo {
     public class BatteryImpl : AbstractBatteryImpl {
 
         public BatteryImpl() {
+            this.UpdateStats();
+            Battery.AggregateBattery.ReportUpdated += (sender, args) => this.UpdateStats();
+        }
+
+
+        void UpdateStats() {
+            var stats = Battery.AggregateBattery.GetReport();
+            this.IsCharging = stats.Status != BatteryStatus.Discharging;
+
+            if (stats.RemainingCapacityInMilliwattHours != null && stats.FullChargeCapacityInMilliwattHours != null)
+                this.Percentage = stats.RemainingCapacityInMilliwattHours.Value / stats.FullChargeCapacityInMilliwattHours.Value;
         }
     }
 }
