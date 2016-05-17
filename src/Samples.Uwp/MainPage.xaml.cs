d9878230bd3e99b31f4890837b10c0dce572d4ba
Allan Ritchie <allan.ritchie@gmail.com>
2016-05-17 09:28:02 -0400

    minor

--- src/Samples.Uwp/MainPage.xaml.cs
+++ src/Samples.Uwp/MainPage.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Xamarin.Forms.Platform.WinRT;
+using Xamarin.Forms.Platform.UWP;
 
 
 namespace Samples.Uwp
@@ -7,9 +7,11 @@ namespace Samples.Uwp
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : WindowsPage {
+    public sealed partial class MainPage : WindowsPage
+    {
 
-        public MainPage() {
+        public MainPage()
+        {
             InitializeComponent();
             this.LoadApplication(new Samples.App());
         }
