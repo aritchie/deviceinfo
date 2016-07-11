6a064a01d5e8f3af6a5dc5df7565d532ca879b44
Allan Ritchie <allan.ritchie@gmail.com>
2016-07-11 09:45:00 -0400

    add uwp connectivity

--- src/Acr.DeviceInfo.Uwp/ConnectivityImpl.cs
+++ src/Acr.DeviceInfo.Uwp/ConnectivityImpl.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Net.NetworkInformation;
+using System.Reactive.Linq;
 using Windows.Networking.Connectivity;
 
 
@@ -8,80 +9,71 @@ namespace Acr.DeviceInfo
 {
     public class ConnectivityImpl : IConnectivity
     {
-
-        //public ConnectivityImpl() {
-        //    NetworkInformation.NetworkStatusChanged += args => this.SetState();
-        //    this.SetState();
-        //}
+        public string CellularNetworkCarrier { get; } = null;
 
 
-        //void SetState() {
-        //    var avail = NetworkInterface.GetIsNetworkAvailable();
+        public string IpAddress => NetworkInformation
+                .GetHostNames()
+                .Last()
+                .DisplayName;
 
-        //    if (avail)
-        //        this.InternetReachability = ConnectionStatus.NotReachable;
-        //    else {
-        //        var profile = NetworkInformation.GetInternetConnectionProfile();
+        public NetworkReachability InternetReachability
+        {
+            get
+            {
+                var avail = NetworkInterface.GetIsNetworkAvailable();
 
-        //        switch (profile.NetworkAdapter.IanaInterfaceType) {
-        //            case 71:
-        //                this.InternetReachability = ConnectionStatus.ReachableViaWifi;
-        //                break;
+                if (!avail)
+                    return NetworkReachability.NotReachable;
 
-        //            case 243:
-        //            case 244:
-        //                this.InternetReachability = ConnectionStatus.ReachableViaCellular;
-        //                break;
+                var profile = NetworkInformation.GetInternetConnectionProfile();
+                switch (profile.NetworkAdapter.IanaInterfaceType)
+                {
+                    case 71:
+                        return NetworkReachability.Wifi;
 
-        //            default:
-        //                this.InternetReachability = ConnectionStatus.ReachableViaOther;
-        //                break;
-        //        }
-        //        //switch (NetworkInterface.NetworkInterfaceType) {
-        //        //    case NetworkInterfaceType.Wireless80211:
-        //        //        this.InternetReachability = ConnectionStatus.ReachableViaWifi;
-        //        //        break;
+                    case 243:
+                    case 244:
+                        return NetworkReachability.Cellular;
 
-        //        //    case NetworkInterfaceType.MobileBroadbandCdma:
-        //        //    case NetworkInterfaceType.MobileBroadbandGsm:
-        //        //        this.InternetReachability = ConnectionStatus.ReachableViaCellular;
-        //        //        break;
-        //        //}
-        //    }
-        //}
+                    default:
+                        return NetworkReachability.Other;
+                }
+                //switch (NetworkInterface.NetworkInterfaceType) {
+                //    case NetworkInterfaceType.Wireless80211:
+                //        this.InternetReachability = ConnectionStatus.ReachableViaWifi;
+                //        break;
 
-
-        //protected override string GetIpAddress() {
-        //    return NetworkInformation
-        //        .GetHostNames()
-        //        .Last()
-        //        .DisplayName;
-        //}
+                //    case NetworkInterfaceType.MobileBroadbandCdma:
+                //    case NetworkInterfaceType.MobileBroadbandGsm:
+                //        this.InternetReachability = ConnectionStatus.ReachableViaCellular;
+                //        break;
+                //}
+            }
+        }
 
 
-        //protected override string GetNetworkCarrier()
-        //{
-        //    return null;
-        //}
+        public string WifiSsid
+        {
+            get
+            {
+                var profile = NetworkInformation.GetInternetConnectionProfile();
+                if (!profile.IsWlanConnectionProfile)
+                    return null;
+
+                return profile.WlanConnectionProfileDetails.GetConnectedSsid();
+            }
+        }
 
 
-        //protected override string GetWifiSsid()
-        //{
-        //    var profile = NetworkInformation.GetInternetConnectionProfile();
-        //    if (!profile.IsWlanConnectionProfile)
-        //        return null;
-
-        //    return profile.WlanConnectionProfileDetails.GetConnectedSsid();
-        //}
-
-        public bool IsInternetAvailable { get; }
-        public NetworkReachability InternetReachability { get; }
-        public string CellularNetworkCarrier { get; }
-        public string IpAddress { get; }
-        public string WifiSsid { get; }
         public IObservable<NetworkReachability> WhenStatusChanged()
         {
-            throw new NotImplementedException();
+            return Observable.Create<NetworkReachability>(ob =>
+            {
+                var handler = new NetworkStatusChangedEventHandler(sender => ob.OnNext(this.InternetReachability));
+                NetworkInformation.NetworkStatusChanged += handler;
+                return () => NetworkInformation.NetworkStatusChanged -= handler;
+            });
         }
     }
 }
