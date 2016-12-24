6e34acfcbc36486b9597a3f37c476445b7fa3945
Allan Ritchie <allan.ritchie@gmail.com>
2016-12-24 16:17:21 -0500

    fix #18 - multiply by scale to get proper resolution

--- src/Acr.DeviceInfo.iOS/HardwareImpl.cs
+++ src/Acr.DeviceInfo.iOS/HardwareImpl.cs
@@ -1,18 +1,14 @@
 ﻿using System;
-using System.Threading.Tasks;
-using CoreBluetooth;
-using CoreFoundation;
 using ObjCRuntime;
 using UIKit;
 
 
 namespace Acr.DeviceInfo
 {
-
     public class HardwareImpl : IHardware
     {
-        public int ScreenHeight { get; } = (int)UIScreen.MainScreen.Bounds.Height;
-        public int ScreenWidth { get; } = (int)UIScreen.MainScreen.Bounds.Width;
+        public int ScreenHeight { get; } = (int)UIScreen.MainScreen.Bounds.Height * (int)UIScreen.MainScreen.Scale;
+        public int ScreenWidth { get; } = (int)UIScreen.MainScreen.Bounds.Width * (int)UIScreen.MainScreen.Scale;
         public string DeviceId { get; } = UIDevice.CurrentDevice.IdentifierForVendor.AsString();
         public string Manufacturer { get; } = "Apple";
         public string Model { get; } = UIDevice.CurrentDevice.Model;
@@ -20,48 +16,5 @@ namespace Acr.DeviceInfo
         public bool IsSimulator { get; } = (Runtime.Arch == Arch.SIMULATOR);
         public bool IsTablet { get; } = UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Pad;
         public OperatingSystemType OS { get; } = OperatingSystemType.iOS;
-
-
-        public Task<bool> HasFeature(Feature feature)
-        {
-            return Task.FromResult(this.Has(feature));
-        }
-
-
-        bool Has(Feature feature)
-        {
-            switch (feature)
-            {
-                case Feature.Camera:
-                    return UIImagePickerController.IsCameraDeviceAvailable(UIImagePickerControllerCameraDevice.Front);
-
-                case Feature.CameraBack:
-                    return UIImagePickerController.IsCameraDeviceAvailable(UIImagePickerControllerCameraDevice.Rear);
-
-                case Feature.CameraFront:
-                    return UIImagePickerController.IsCameraDeviceAvailable(UIImagePickerControllerCameraDevice.Rear) ||
-                           UIImagePickerController.IsCameraDeviceAvailable(UIImagePickerControllerCameraDevice.Front);
-
-                case Feature.Bluetooth:
-                    return false;
-
-                case Feature.BluetoothLE:
-                    using (var cb = new CBCentralManager(DispatchQueue.DefaultGlobalQueue))
-                    {
-                        switch (cb.State)
-                        {
-                            case CBCentralManagerState.Unknown:
-                            case CBCentralManagerState.Unsupported:
-                                return false;
-
-                            default:
-                                return true;
-                        }
-                    }
-
-                default:
-                    return false;
-            }
-        }
     }
 }
