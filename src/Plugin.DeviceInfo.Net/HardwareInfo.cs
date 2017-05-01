98b65c94657c67de9389a24072451a0509f3c5fb
Allan Ritchie <allan.ritchie@gmail.com>
2017-05-01 09:30:16 -0400

    add some additional properties for net45 provider for hardware

--- src/Plugin.DeviceInfo.Net/HardwareInfo.cs
+++ src/Plugin.DeviceInfo.Net/HardwareInfo.cs
@@ -8,29 +8,13 @@ namespace Plugin.DeviceInfo
     public class HardwareInfo : IHardwareInfo
     {
 
-        public HardwareInfo()
-        {
-
-            //var mos = new ManagementObjectSearcher("SELECT * FROM Win32_OperatingSystem");
-            //var obj = mos.Get().Cast<ManagementObject>().FirstOrDefault();
-
-            //this.Manufacturer = obj["Manufacturer"].ToString();
-            //this.Model = obj["model"].ToString();
-            //this.OperatingSystem = $"{obj["Caption"]} - {obj["Version"]}";
-
-            //mos = new ManagementObjectSearcher("SELECT ProcessorId FROM Win32_Processor");
-            //obj = mos.Get().Cast<ManagementObject>().FirstOrDefault();
-            //this.DeviceId = obj["ProcessorId"].ToString();
-        }
-
-
         public int ScreenHeight { get; } = SystemInformation.VirtualScreen.Height;
         public int ScreenWidth { get; } = SystemInformation.VirtualScreen.Width;
         public string DeviceId { get; }
         public string Manufacturer { get; }
         public string Model { get; }
-        public string OperatingSystem { get; }
-        public string OperatingSystemVersion { get; }
+        public string OperatingSystem => Environment.OSVersion.Platform.ToString();
+        public string OperatingSystemVersion => Environment.OSVersion.VersionString;
         public bool IsSimulator { get; } = false;
         public bool IsTablet { get; } = false;
     }
